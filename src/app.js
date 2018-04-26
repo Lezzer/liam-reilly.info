37af540baacbef3b1feb33843c61e99ee5917c4c
Liam Reilly <liam.reilly.1@gmail.com>
2018-04-26 21:17:25 +0100

    Tweaked import for css in app

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Header from "./header";
 import Content from "./content";
 import Footer from "./footer";
-import css from './styles/app.css'
+import './styles/app.css'
 import { BrowserRouter as Router } from 'react-router-dom';
 
 export default class App extends Component {
