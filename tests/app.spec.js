b7efab3c15075885d688df8c173eb26073138c00
Liam Reilly <liam.reilly.1@gmail.com>
2018-04-26 13:42:49 +0100

    Added app tests

---
+++ tests/app.spec.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import App from "../src/app";
+import Header from '../src/header';
+import Content from '../src/content';
+import Footer from '../src/footer';
+import { shallow } from 'enzyme';
+import '../tests/setup';
+
+describe('the app renders as expected', () => {
+
+    let wrapper;
+    
+    beforeAll(() => {
+        wrapper = shallow(<App />);
+    });
+
+    it('renders without crashing', () => {
+        expect(wrapper.length).toBe(1);
+    });
+
+    it('should contain a header', () => {
+        const header = wrapper.find(Header);
+
+        expect(header.length).toBe(1);
+    });
+
+    it('should contain a content', () => {
+        const content = wrapper.find(Content);
+
+        expect(content.length).toBe(1);
+    });
+    
+    it('should contain a footer', () => {
+        const footer = wrapper.find(Footer);
+
+        expect(footer.length).toBe(1);
+    });
+});
