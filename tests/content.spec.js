69758b5c251a922ffde12d595d93832a3b565cb3
Liam Reilly <liam.reilly.1@gmail.com>
2018-04-26 13:24:37 +0100

    Added tests to ensure correct content component renders on route

--- tests/content.spec.js
+++ tests/content.spec.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import Content from '../src/content';
 import { shallow } from 'enzyme';
+import Home from '../src/home';
+import Resume from '../src/resume';
+import Blog from '../src/blog';
+import Contact from '../src/contact';
+
 import '../tests/setup';
 
 describe('the content section renders as expected', () => {
@@ -9,5 +15,21 @@ describe('the content section renders as expected', () => {
         const wrapper = shallow(<Content />);
 
         expect(wrapper.length).toBe(1);
+    });
+
+    it('renders correct components for routes', () => {
+        const wrapper = shallow(<Content/>);
+        const pathMap = wrapper
+            .find(Route)
+            .reduce((pathMap, route) => {
+                const routeProps = route.props();
+                pathMap[routeProps.path] = routeProps.component;
+                return pathMap;
+            }, {});
+
+        expect(pathMap['/']).toBe(Home);
+        expect(pathMap['/resume/']).toBe(Resume);
+        expect(pathMap['/blog/']).toBe(Blog);
+        expect(pathMap['/contact/']).toBe(Contact);
     });
 });
