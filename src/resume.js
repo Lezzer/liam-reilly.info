df37a42a23616b5e848c7373cacb4251e6defccf
Liam Reilly <liam.reilly.1@gmail.com>
2018-05-01 20:31:09 +0100

    Added font awesome (react style)

--- src/resume.js
+++ src/resume.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-import './styles/resume.css';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { faEnvelope, faPhone, faMapMarker } from '@fortawesome/fontawesome-free-solid';
+
+import css from './styles/resume.css';
 
 export default class Resume extends Component
 {
     render() {
         return (
-            <div id={"page"}>
-                <h1>liam reilly</h1>
+            <div className={css.page}>
+                <div className={css["contact-info"]}>
+                    <h1>liam reilly</h1>
+
+                    <FontAwesomeIcon icon={faEnvelope} />
+                    <span>offers@liam-reilly.info</span>
+
+                    <FontAwesomeIcon icon={faPhone} />
+                    <span>07802 385 455</span>
+
+                    <FontAwesomeIcon icon={faMapMarker} />
+                    <span>stretford</span>
+                </div>
             </div>
         );
     }
