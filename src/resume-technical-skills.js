e07b4a864f66d51cb2e539b3f9256a3a8735e45f
Liam Reilly <liam.reilly.1@gmail.com>
2018-05-04 15:04:13 +0100

    Added basic styling to a bunch of areas. Will be changing

--- src/resume-technical-skills.js
+++ src/resume-technical-skills.js
@@ -11,10 +11,11 @@ export default class ResumeTechnicalSkills extends Component{
 
                 <ul>
                     <li>Languages: <span>C#, VB, Python, PHP</span></li>
-                    <li>Web: <span>HTML, CSS, JavaScript (including JQuery, Angular, Typescript &amp KnockoutJS), ASP.NET MVC &amp WebAPI</span></li>
-                    <li>Databases: <span>MS SQL, PostgreSQL & MySQL</span></li>
-                    <li>Environments: <span>Linux &amp Windows</span></li>
-                    <li>Version control: <span>Git &amp Mercurial</span></li>
+                    <li>Web: <span>React, Redux, Webpack, Angular, ES6, Typescript, KnockoutJS, ASP.NET MVC &amp WebAPI</span></li>
+                    <li>Databases: <span>DynamoDB, MS SQL, PostgreSQL & MySQL</span></li>
+                    <li>Operating Systems: <span>Linux, Windows &amp  Mac OS</span></li>
+                    <li>Platforms: <span>Docker</span></li>
+                    <li>Version control: <span>Git/Mercurial, </span></li>
                     <li>Microsoft Technologies: <span>WCF, WPF &amp Win/Web Forms</span></li>
                     <li>Messaging: <span>RabbitMQ, Azure Service Bus, Celery &amp MassTransit</span></li>
                     <li>DevOps: <span>Puppet, Ansible, Docker, Vagrant &amp Octopus Deploy</span>
