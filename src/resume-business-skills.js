8cf1c069269230b062f98ae4ec239989f0158597
Liam Reilly <liam.reilly.1@gmail.com>
2018-05-05 20:18:22 +0100

    Few more teaks to resume

--- src/resume-business-skills.js
+++ src/resume-business-skills.js
@@ -12,9 +12,9 @@ export default class ResumeBusinessSkills extends Component{
                 <ul>
                     <li>Software Development: <span>Creative and technical expertise in software development, leading projects from conception to completion through the full development life cycle, with adherence to SOLID development principles.</span></li>
                     <li>Project Planning: <span>Thorough working knowledge of software development life cycle methodologies. Extensive experience running Kanban and Agile Scrum sprints. Delivering all sprint ceremonies.  </span></li>
-                    <li>Team Working: <span>Positively work with others and easily build and maintain relationships to ensure the success of the team. Experience of managing expectations and deliverables across teams in various departments.</span></li>
+                    <li>Team Working: <span>Positively work with others, much experience using pair/mob programming approaches.  Easily build and maintain relationships to ensure the success of the team.  Experience of managing expectations and deliverables across teams in various departments.</span></li>
                     <li>Customer Focus: <span>Ability to work positively with internal and external clients and partners. Interacting with the business, clients, suppliers and developers to ensure the needs of all parties are met.</span></li>
-                    <li>People management: <span>A supportive, mentor to junior team members, developing skills, training and providing code reviews. </span></li>
+                    <li>People management: <span>A supportive, mentor to other team members, developing skills, training and providing code reviews. </span></li>
                 </ul>
             </div>
         );
