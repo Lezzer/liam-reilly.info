e59a66d6dacc3205ef10d634a68d0404d120f534
Liam Reilly <liam.reilly.1@gmail.com>
2018-05-05 13:53:11 +0100

    Updated business skills

--- src/resume-business-skills.js
+++ src/resume-business-skills.js
@@ -10,11 +10,11 @@ export default class ResumeBusinessSkills extends Component{
                 <h2>business skills</h2>
 
                 <ul>
-                    <li>Software Development: <span>Creative and technical expertise in software development, leading projects from conception to completion through the full development life cycle, with adherence to SOLID development principles. Experience of delivering peer reviews and code reviews. </span></li>
-                    <li>Project Planning: <span>Thorough working knowledge of software development life cycle methodologies. Instrumental in implementing both Scrum and XP practices to our in-house development style, enabling delivery of high quality work to tight deadlines.</span></li>
+                    <li>Software Development: <span>Creative and technical expertise in software development, leading projects from conception to completion through the full development life cycle, with adherence to SOLID development principles.</span></li>
+                    <li>Project Planning: <span>Thorough working knowledge of software development life cycle methodologies. Extensive experience running Kanban and Agile Scrum sprints. Delivering all sprint ceremonies.  </span></li>
                     <li>Team Working: <span>Positively work with others and easily build and maintain relationships to ensure the success of the team. Experience of managing expectations and deliverables across teams in various departments.</span></li>
-                    <li>Customer Focus: <span>Ability to work positively with internal and external clients and partners.  Interacting with the business, clients, suppliers and developers to ensure the needs of all parties are met.</span></li>
-                    <li>People management: <span>A supportive, mentor to junior team members, developing skills and training.</span></li>
+                    <li>Customer Focus: <span>Ability to work positively with internal and external clients and partners. Interacting with the business, clients, suppliers and developers to ensure the needs of all parties are met.</span></li>
+                    <li>People management: <span>A supportive, mentor to junior team members, developing skills, training and providing code reviews. </span></li>
                 </ul>
             </div>
         );
