01abfd6404a8e8868aa624f147093822e4c25ce2
Liam Reilly <liam.reilly.1@gmail.com>
2018-05-11 11:45:03 +0100

    Added job-history-item tests

---
+++ tests/resume-job-history-item.spec.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import '../tests/setup';
+
+import ResumeJobHistoryItem from "../src/resume-job-history-item";
+
+describe('the resume statement component renders as expected', () => {
+
+    const wrapper = shallow(<ResumeJobHistoryItem
+        company={"liam-reilly.info"}
+        location={"Manchester"}
+        start={"2018"}
+        end={"Present"}
+        position={"Owner"} />);
+
+
+    it('renders without crashing', () => expect(wrapper.length).toBe(1));
+    
+    it('contains an unordered list with 3 list items', () => {
+       
+        const ul = wrapper.find(".job_history_item ul");
+        const items = ul.children().length;
+        expect(items).toBe(3);
+    });
+    
+    it('contains a span', () => {
+        const span = wrapper.find('.job_history_item span');
+        expect(span.length).toBe(1);
+    })
+});
