802470663031fb6156bccb123cbe6779ff9f11b4
Liam Reilly <liam.reilly.1@gmail.com>
2018-05-11 12:16:35 +0100

    Added tests for resume-technical-skills

---
+++ tests/resume-technical-skills.spec.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import '../tests/setup';
+
+import ResumeTechnicalSkills from "../src/resume-technical-skills";
+
+describe('the resume statement component renders as expected', () => {
+
+    const wrapper = shallow(<ResumeTechnicalSkills/>);
+
+
+    it('renders without crashing', () => expect(wrapper.length).toBe(1));
+
+
+    it('contains a header', () => {
+        const header = wrapper.find('.technical_skills h2');
+        expect(header.length).toBe(1);
+    });
+
+    it('contains an unordered list with 10x list items', () => {
+
+        const ul = wrapper.find(".technical_skills ul");
+        const items = ul.children().length;
+        expect(items).toBe(10);
+    });
+});
