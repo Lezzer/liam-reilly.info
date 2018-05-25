bb22e6d3623843e1c8fcb39069644d851d1f4576
Liam Reilly <liam.reilly.1@gmail.com>
2018-05-25 13:41:52 +0100

    Updated content on home page

--- src/home.js
+++ src/home.js
@@ -12,8 +12,10 @@ export default class Home extends Component
                     title={"At long last... liam-reilly.info is born!"}
                     content={[
                         "After much procrastination, the website is finally here!",
-                        "Not much to see just yet, but blog posts will be coming thick and fast!",
-                        "Feel free to have a look around, and get in touch to talk about projects :)",
+                        "Not much to see just yet, as I am still busy getting things ready.",
+                        "My resume is available to view & print directly from the website, and the contact page has all my online social accounts / email etc.",
+                        "I will be working frantically on blog post content, so this will be appearing thick and fast!",
+                        "Feel free to have a look around, and get in touch :)",
                         "Cheers, Liam"
                     ]}
                 />
