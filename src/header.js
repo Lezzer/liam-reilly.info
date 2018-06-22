f3fea658058ac6d5ea5748341e23bd23ecc5aa42
Liam Reilly <liam.reilly.1@gmail.com>
2018-06-22 13:38:46 +0100

    Fixed unsecure urls

--- src/header.js
+++ src/header.js
@@ -7,7 +7,7 @@ export default class Header extends Component
     render() {
         return (
             <div className={css.header}>
-                <img id="avatar" src='http://www.gravatar.com/avatar/7810a5172f51c37bcba95f5055b0d57e?s=175' /> 
+                <img id="avatar" src='https://www.gravatar.com/avatar/7810a5172f51c37bcba95f5055b0d57e?s=175' /> 
                 <MenuBar />
             </div>
         );
