c71e01bbe4b25b23d04f1f818d67c11275216344
Liam Reilly <liam.reilly.1@gmail.com>
2018-06-27 15:00:14 +0100

    Now servers all routes not just /

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 app.set('port', port);
 
 
-app.get('/', (req, res) => res.status(200).sendFile(__dirname + "/index.html"));
+app.get('*', (req, res) => res.status(200).sendFile(__dirname + "/index.html"));
 
 app.listen(app.get('port'), function () {
     console.log('listening...on port ' + port + ' - ' + staticPath);
