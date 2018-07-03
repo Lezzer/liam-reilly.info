88798a322a6cdff91b7971efa357c072af81eec8
Liam Reilly <liam.reilly.1@gmail.com>
2018-07-03 22:59:22 +0100

    Fixed statment layout at 4k and updated tech skills

--- src/components/Resume/TechSkills/resume-technical-skills.js
+++ src/components/Resume/TechSkills/resume-technical-skills.js
@@ -12,14 +12,14 @@ export default class ResumeTechnicalSkills extends Component{
                 <ul>
                     <li>Cloud: <span>Amazon Web Services, Microsoft Azure</span></li>
                     <li>Languages: <span>C# (Full .NET and .NET Core 2)</span></li>
-                    <li>Web: <span>React, Redux, GraphQL, Webpack, ES6, ASP.NET MVC, WebAPI</span></li>
+                    <li>Web: <span>React, Redux, GraphQL, Webpack, ES6, ASP.NET MVC, WebAPI, WCF</span></li>
                     <li>Databases: <span>DynamoDB, MS SQL</span></li>
-                    <li>Messaging: <span>SQS, Azure Service Bus</span></li>
-                    <li>Testing: <span>NUnit, SpecFlow, Jest, Enzyme, Stubby, Mountebank</span></li>
+                    <li>Messaging: <span>SQS, SNS, Azure Service Bus</span></li>
+                    <li>Testing: <span>NUnit, SpecFlow, Jest, Enzyme, Stubby</span></li>
                     <li>DevOps: <span>Terraform, Docker, Vagrant, Rancher</span></li>
-                    <li>CI/CD: <span>GoCD</span></li>
+                    <li>CI/CD: <span>CircleCI, GoCD</span></li>
                     <li>Version control: <span>Git</span></li>
-                    <li>Desktop: <span>WCF, WPF</span></li>
+                    <li>Desktop: <span>WPF</span></li>
                 </ul>
                 
                 <p>** The list above only includes tech used daily in my current role **</p>
