aaafcc0a5e7ca4c05f82fdb410ab37b23f43e2f2
Liam Reilly <liam.reilly.1@gmail.com>
2018-11-22 16:07:39 +0000

    Blog intro typo

--- src/components/Blog/blog.js
+++ src/components/Blog/blog.js
@@ -11,7 +11,7 @@ export default class Blog extends Component
                     day={"21"} 
                     title={"Why I love ten percent time!"}
                     content={[
-                        "You may or may not know that I recently changed jobs.  I'm now a Principal Software Engineer now at Ditto Music.",
+                        "You may or may not know that I recently changed jobs.  I'm now a Principal Software Engineer at Ditto Music.",
                         "I was brought in, along with a couple of other friends (we've all worked together previously) to form a new team to build a completely new cloud infrastructure in AWS, and design and build all new systems/services to allow the company to grow and scale."
                     ]}
                     link={"/blog/00-ten-percent-time-intro.md"}
