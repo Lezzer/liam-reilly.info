04119714591591d155ba19c42f90bea550b833a3
Liam Reilly <liam.reilly.1@gmail.com>
2019-01-17 11:16:10 +0000

    Added scripts which will be used to apply terraform

---
+++ infrastructure/terraform/scripts/terraform-plan.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+KEY="aws_access_key=$AWS_ACCESS_KEY_ID"
+SECRET="aws_secret_key=${AWS_SECRET_KEY}"
+
+terraform plan -input=false -out=terraform.plan -var ${KEY} -var ${SECRET}
