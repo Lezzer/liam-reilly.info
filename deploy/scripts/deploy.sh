8fa3cecfa9c72eae4b700d20e2e7ab6717572e89
Liam Reilly <liam.reilly.1@gmail.com>
2019-09-14 14:19:41 +0100

    Updated script

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 npm run build
-cd ../../dist
-aws s3 sync . s3://liam-reilly.info
+aws s3 sync ./dist s3://liam-reilly.info
 #aws cloudfront create-invalidation --distribution-id E217GY0T80SPQ5
