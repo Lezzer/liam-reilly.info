ab1f12c6520cd5d83aaf8f7a96af195993de92a8
Liam Reilly <liam.reilly.1@gmail.com>
2019-09-14 13:40:48 +0100

    Updated deploy script

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-print $1
 cd ../../dist
 
-print "Deploy script called with $1"
-
-#aws s3 sync s3://liam-reilly.info
-#aws cloudfront create-invalidation --distribution-id E7HHZDNHPXBIG
+aws s3 sync s3://liam-reilly.info
+aws cloudfront create-invalidation --distribution-id E217GY0T80SPQ5
