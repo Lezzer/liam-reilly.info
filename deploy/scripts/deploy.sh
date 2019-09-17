4e93e7058c8c2ea2bc062f88354295fadb18d4b7
Liam Reilly <liam.reilly.1@gmail.com>
2019-09-17 13:37:42 +0100

    Typo in deploy

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -5,8 +5,8 @@ npm run build
 cp ./dist/index.html ./dist/resume.html
 cp ./dist/index.html ./dist/contact.html
 
-mv ./dist resume.html ./dist/resume
-mv ./dist contact.html ./dist/contact
+mv ./dist/resume.html ./dist/resume
+mv ./dist/contact.html ./dist/contact
 
 aws s3 sync ./dist s3://liam-reilly.info
 aws cloudfront create-invalidation --distribution-id E217GY0T80SPQ5 --paths /*
