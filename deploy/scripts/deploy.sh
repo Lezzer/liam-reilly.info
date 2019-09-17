deec996fce70efeb6e2302c837d4415da1773ed6
Liam Reilly <liam.reilly.1@gmail.com>
2019-09-17 12:40:08 +0100

    Added extra step to ensure a file exists when accessing via /resume /contact

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 npm run build
+
+cp ./dist/index.html ./dist/resume
+cp ./dist/index.html ./dist/contact
+
 aws s3 sync ./dist s3://liam-reilly.info
 aws cloudfront create-invalidation --distribution-id E217GY0T80SPQ5 --paths /*
