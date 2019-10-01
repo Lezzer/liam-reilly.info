7d64f54cf6edc9d1d9a7e586743945a03258f32e
Liam Reilly <liam.reilly.1@gmail.com>
2019-10-01 17:50:10 +0100

    Updated keybase name to @

--- deploy/terraform/eu-west-1/dns.tf
+++ deploy/terraform/eu-west-1/dns.tf
@@ -53,7 +53,7 @@ resource "aws_route53_record" "email_record" {
 
 resource "aws_route53_record" "keybase" {
   zone_id = data.aws_route53_zone.website_hosted_zone.zone_id
-  name    = "keybase"
+  name    = "@"
   type    = "TXT"
   ttl     = 300
   records = [
