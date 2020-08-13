1e3f9e3aef270ef1a5cd27b0652b63c976737817
Liam Reilly <liam@alchemistlabs.tech>
2020-08-13 23:14:59 +0100

    Updated terraform for ssl

--- deploy/terraform/eu-west-1/ssl.tf
+++ deploy/terraform/eu-west-1/ssl.tf
@@ -2,19 +2,28 @@ resource "aws_acm_certificate" "cert" {
   domain_name               = var.dns_zone
   subject_alternative_names = ["*.${var.dns_zone}"]
   validation_method         = "DNS"
-  provider                  = "aws.us-east-1"
+  provider                  = aws.us-east-1
 }
 
 resource "aws_route53_record" "cert_validation" {
-  name    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_type
-  zone_id = data.aws_route53_zone.website_hosted_zone.zone_id
-  records = [aws_acm_certificate.cert.domain_validation_options[0].resource_record_value]
-  ttl     = 60
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  zone_id         = data.aws_route53_zone.website_hosted_zone.zone_id
+  name            = each.value.name
+  type            = each.value.type
+  records         = [each.value.record]
+  ttl             = 60
 }
 
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.cert.arn
-  provider                = "aws.us-east-1"
-  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+  provider                = aws.us-east-1
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
