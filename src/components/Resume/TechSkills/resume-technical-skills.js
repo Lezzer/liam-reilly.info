124bc4948e301b887b92d4260bb32a8bbc6b8a8b
Liam Reilly <liam@alchemistlabs.tech>
2020-08-13 23:00:56 +0100

    Updated tech skills on resume

--- src/components/Resume/TechSkills/resume-technical-skills.js
+++ src/components/Resume/TechSkills/resume-technical-skills.js
@@ -10,16 +10,15 @@ export default class ResumeTechnicalSkills extends Component{
                 <h2>technical skills</h2>
                 
                 <ul>
-                    <li>Cloud Infrastructure: <span>Amazon Web Services</span></li>
-                    <li>Languages: <span>C# (.NET Core 3.1)</span></li>
-                    <li>Web: <span>React, NextJS/Gatsby, GraphQL, Webpack, ES6, ASP.NET MVC, WebAPI</span></li>
-                    <li>Data Stores: <span>Elasticsearch, Elasticache (Redis), DynamoDB, MSSQL/MySQL (Amazon RDS), S3</span></li>
-                    <li>Serverless: <span>Lambda, Fargate/ECS</span></li>
+                    <li>Cloud Providers: <span>Amazon Web Services, Google Cloud Platform, Azure</span></li>
+                    <li>Backend: <span>C# (.NET Core)</span></li>
+                    <li>Web: <span>React, NextJS/Gatsby, GraphQL, ES6, ASP.NET MVC</span></li>
+                    <li>Data Stores: <span>DynamoDB, Elasticsearch, MSSQL/MySQL, PostgreSQL</span></li>
+                    <li>Serverless: <span>Lambda, Fargate</span></li>
+                    <li>Container Orchestration: <span>Kubernetes, ECS, Docker</span></li>
                     <li>Messaging: <span>SQS, SNS</span></li>
-                    <li>Testing: <span>NUnit, Jest, Enzyme, Stubby</span></li>
-                    <li>DevOps: <span>Terraform, Docker</span></li>
-                    <li>CI/CD: <span>CircleCI, GoCD, Jenkins</span></li>
-                    <li>Version control: <span>Git</span></li>
+                    <li>Testing: <span>NUnit, Jest, React Testing Library</span></li>
+                    <li>Dev/Git Ops: <span>Terraform, CircleCI, GoCD, ArgoCD, Jenkins</span></li>
                 </ul>
                 
                 <p>** The list above is limited to more recently used technologies and is by no means exhaustive**</p>
--- tests/components/Resume/TechSkills/resume-technical-skills.spec.js
+++ tests/components/Resume/TechSkills/resume-technical-skills.spec.js
@@ -8,19 +8,17 @@ describe('the resume statement component renders as expected', () => {
 
     const wrapper = shallow(<ResumeTechnicalSkills/>);
 
-
     it('renders without crashing', () => expect(wrapper.length).toBe(1));
-
 
     it('contains a header', () => {
         const header = wrapper.find('.technical_skills h2');
         expect(header.length).toBe(1);
     });
 
-    it('contains an unordered list with 10x list items', () => {
+    it('contains an unordered list with 9x list items', () => {
 
         const ul = wrapper.find(".technical_skills ul");
         const items = ul.children().length;
-        expect(items).toBe(10);
+        expect(items).toBe(9);
     });
 });
