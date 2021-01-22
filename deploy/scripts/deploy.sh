6405a5bbc3ebaaef9da5a68244faaa01f0f2d245
Liam Reilly <liam.reilly.1@gmail.com>
2021-01-22 23:57:34 +0000

    Updated deployment script

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+
 npm run build
 
 cp ./dist/index.html ./dist/resume.html
 cp ./dist/index.html ./dist/contact.html
-cp ./dist/index.html ./dist/error.html
+
+mv ./dist/resume.html ./dist/resume
+mv ./dist/contact.html ./dist/contact
 
 aws s3 sync ./dist s3://liam-reilly.info
 aws cloudfront create-invalidation --distribution-id EUS4UKP82XSQ5 --paths /*
--- src/server.js
+++ src/server.js
@@ -6,11 +6,9 @@ const app = express();
 const staticPath = path.join(__dirname, '/');
 app.use(express.static(staticPath));
 
-
 const port = process.env.PORT || 3000;
 
 app.set('port', port);
-
 
 app.get('*', (req, res) => res.status(200).sendFile(__dirname + "/index.html"));
 
