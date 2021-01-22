a36bb38e2829fef672e279f2f48d683f956d73e2
Liam Reilly <liam.reilly.1@gmail.com>
2021-01-22 23:27:06 +0000

    Updated deployment script

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
-
 npm run build
 
 cp ./dist/index.html ./dist/resume.html
 cp ./dist/index.html ./dist/contact.html
-
-mv ./dist/resume.html ./dist/resume
-mv ./dist/contact.html ./dist/contact
+cp ./dist/index.html ./dist/error.html
 
 aws s3 sync ./dist s3://liam-reilly.info
 aws cloudfront create-invalidation --distribution-id EUS4UKP82XSQ5 --paths /*
