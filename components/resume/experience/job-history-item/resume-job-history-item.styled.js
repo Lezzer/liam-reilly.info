bb782b49be9b2f1a6f3f2cefd042faf46f872b66
Liam Reilly <liam@alchemistlabs.tech>
2021-01-26 23:26:36 +0000

    finished resume

--- components/resume/experience/job-history-item/resume-job-history-item.styled.js
+++ components/resume/experience/job-history-item/resume-job-history-item.styled.js
@@ -1,7 +1,52 @@
 import styled from "styled-components";
+import {device} from "../../../../style/device";
 
 const StyledResumeJobHistoryItem = styled.div`
-  background-color: crimson;
+  width: 100%;
+  text-align: left;
+  font-size: 1.2rem;
+
+  ul {
+    padding: 0;
+    margin-bottom: .52em;
+    font-weight: bold;
+  }
+  
+  li {
+    display: block;
+    list-style: none;
+  }
+  
+  span {
+    color: ${({theme}) => theme.alternativeHighlight};
+  }
+  
+  li:after {
+    content: " ";
+    padding: .2rem;
+    font-size: 1rem;
+    color: ${({theme}) => theme.alternativeHighlight};
+  }
+  
+  li:last-child:after {
+    content: normal;
+  }
+
+  @media only screen and ${device.laptop} {
+    font-size: 1.8rem;
+
+    li {
+      display: inline;
+    }
+    
+    li:after {
+      content: " // ";
+      padding: 1rem;
+      font-size: 3rem;
+    }
+
+  }
+
 `
 
 export default StyledResumeJobHistoryItem
--- components/resume/experience/resume-experience.styled.js
+++ components/resume/experience/resume-experience.styled.js
@@ -1,7 +1,33 @@
 import styled from "styled-components";
+import {device} from "../../../style/device";
 
 const StyledResumeExperience = styled.section`
-  background-color: coral;
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  font-weight: 300;
+
+  h2 {
+    font-size: 2rem;
+    font-weight: 400;
+    text-align: center;
+    text-transform: uppercase;
+    margin: 0;
+  }
+
+  @media only screen and ${device.laptop} {
+    width: 90%;
+
+    p {
+      font-size: 1.4rem;
+    }
+
+    h2 {
+      font-size: 3rem;
+      font-weight: 400;
+      margin: 2rem;
+    }
+  }
 `
 
 export default StyledResumeExperience
