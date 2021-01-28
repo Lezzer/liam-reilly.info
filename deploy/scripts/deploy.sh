ef23878f5631833473ff890f15a8b448bd800e15
Liam Reilly <liam@alchemistlabs.tech>
2021-01-28 14:41:57 +0000

    Deploy script

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 npm run export
 
+cp ./out/contact/index.html ./out/contact
+cp ./out/resume/index.html ./out/resume
+
 aws s3 sync ./out s3://liam-reilly.info
 aws cloudfront create-invalidation --distribution-id EUS4UKP82XSQ5 --paths /*
