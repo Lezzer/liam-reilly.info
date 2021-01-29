37ed106c596e8e8187a96c604fc0ca4961b83420
Liam Reilly <liam@ALCHEMISTLABS-MBP-13-2020-M1.lan>
2021-01-29 00:41:59 +0000

    Updated cloudfront distribution origin id so it becomes a custom origin

--- deploy/terraform/eu-west-1/_vars.tf
+++ deploy/terraform/eu-west-1/_vars.tf
@@ -25,5 +25,5 @@ variable "dns_zone" {
 
 variable "cloudfront_distribution_origin_id" {
   description = "Name of the cloudfront distribution"
-  default     = "liam-reilly.info"
+  default     = "https://www.liam-reilly.info"
 }
