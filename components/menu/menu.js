9b70878fad65c7cc0fddff5ee9ee1fc26ffcb096
Liam Reilly <liam@alchemistlabs.tech>
2021-05-04 11:43:17 +0100

    config:

--- components/menu/menu.js
+++ components/menu/menu.js
@@ -10,17 +10,17 @@ const Menu = () => <StyledMenu>
             </Link>
         </li>
         <li>
-            <Link href="/resume" as="/resume">
+            <Link href="/resume">
             <a>resume</a>
             </Link>
         </li>
         <li>
-            <Link href="/contact" as="/contact">
+            <Link href="/contact">
             <a>contact</a>
             </Link>
         </li>
         <li>
-            <Link href="/books" as="/books">
+            <Link href="/books">
                 <a>books</a>
             </Link>
         </li>
